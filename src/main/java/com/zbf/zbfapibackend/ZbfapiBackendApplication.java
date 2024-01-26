c57abd2f6969b1bed8c07734b3d8fffa4b582092
ZhaoWang <zw949146236@outlook.com>
2024-01-26 22:18:28 +0800

    init

---
+++ src/main/java/com/zbf/zbfapibackend/ZbfapiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zbf.zbfapibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbfapiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZbfapiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zbf/zbfapibackend/ZbfapiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zbf.zbfapibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZbfapiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
